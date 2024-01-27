817c02a831acd79a6999d932051f6cfd1f4da8b5
Jeremy Bise <jeremy.bise@thosegeeks.com>
2024-01-26 21:24:14 -0500

    tailwind maybe

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./content/*.md", "./themes/cookery/layouts/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
